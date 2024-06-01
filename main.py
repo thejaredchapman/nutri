14a19673918d062a7c2080976878e5e1f4c4d36c
Jared Chapman <thejaredchapman@gmail.com>
2024-06-01 15:05:59 -0500

    working

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+
+GENDER = "MALE"
+WEIGHT_KG = 300
+HEIGHT_CM = 170.18
+AGE = 38
+
+
+APP_ID = UPDATE PARAM
+APP_KEY = UPDATE PARAM
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": APP_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+print(result)
